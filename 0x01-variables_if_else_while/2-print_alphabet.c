ad7405f255ea4129befc1239572ff3991a18dc5c
Kingsleyorji <kingsleyorji590@gmail.com>
2023-09-10 15:14:27 -0700

    commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Display letter a to z.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
