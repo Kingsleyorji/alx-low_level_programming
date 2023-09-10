bb8c246fe3d3f0843ccd06efb9d0288670c0db52
Kingsleyorji <kingsleyorji590@gmail.com>
2023-09-10 16:32:54 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Printing numbers in base 10.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d\n", n);
+	}
+	return (0);
+}
