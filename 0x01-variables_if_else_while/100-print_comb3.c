15556673b39eecaeadb570fe4d0c22a43037c5cc
Kingsleyorji <kingsleyorji590@gmail.com>
2023-09-11 06:27:14 -0700

    commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,23 +10,23 @@
 int main(void)
 {
 
-	int m, n;
+        int m, n;
 
-	for (m = '0'; m < '9'; m++)
-	{
-		for (n = '0'; n < '9'; n++)
-		{
-			if (n != m)
-			{
-				putchar(m);
-				putchar(n);
-		if (m == '8' && n == '9')
-			continue;
-		putchar(',');
-		putchar(' ');
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+        for (m = '0'; m < '9'; m++)
+        {
+                for (n = '0'; n < '9'; n++)
+                {
+                        if (n != m)
+                        {
+                                putchar(m);
+                                putchar(n);
+                if (m == '8' && n == '9')
+                        continue;
+                putchar(',');
+                putchar(' ');
+                        }
+                }
+        }
+        putchar('\n');
+        return (0);
 }
