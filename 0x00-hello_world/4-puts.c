f1e8b1ec3855e0446c818ae3e9be91e2146ead1a
Kingsleyorji <kingsleyorji590@gmail.com>
2023-09-12 01:54:46 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	puts("\"programming is like buiding a multilingual puzzle");
 		return (0);
