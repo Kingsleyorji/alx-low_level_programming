7193af4093341364832e36b1735383567f215671
Kingsleyorji <kingsleyorji590@gmail.com>
2023-09-15 05:58:00 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point.
+ *
+ * @a: integer
+ *
+ * @b: integer
+ *
+ * Return: Always 0 (Succes)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 int _isupper(int c);
 int main(void);
 int _isdigit(int c);
+int mul(int a, int b);
